7693b09b116d1326d81e19fb425382c8c8aca58f
TACCmcollins <mcollins@tacc.utexas.edu>
2025-06-16 15:40:27 -0500

    pynumber and pycolor

---
+++ pycolor.py
@@ -0,0 +1,12 @@
+#!/opt/apps/gcc14/cuda12/python3/3.11.8/bin/python3
+
+import random
+
+# List of colors
+colors = ["red", "blue", "green", "yellow", "purple"]
+
+# Randomly pick one color
+random_color = random.choice(colors)
+
+# Display the result
+print("Randomly selected color:", random_color)
---
+++ pynumber.py
@@ -0,0 +1,7 @@
+import random
+
+# Generate a random number between 1 and 5
+random_number = random.randint(1, 5)
+
+# Display the result
+print("Randomly generated number:", random_number)
